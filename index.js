fddfe56fef2c112d696852888f7542566337e577
Rajdip98 <brajdip80@gmail.com>
2022-05-14 13:41:20 +0530

    latest push

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager");
 
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required:true
+        required: true
     },
     age: {
         type: Number,
-        required:true,
-        validate(value){
-            if(value<0){
+        default: 0,
+        validate(value) {
+            if (value < 0) {
                 throw new Error('Age must be Postive')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email does not exists');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (value.length <= 6 || value.toLowerCase() === "password") {
+                throw new Error('Set Password something else')
             }
         }
     }
@@ -20,7 +42,9 @@ const User = mongoose.model('User', {
 
 const me = new User({
     name: "Rajdip Banerjee",
-    age: 24
+    email: 'rajDip@gmail.com',
+    age:24,
+    password:"1234567"
 });
 
 me.save().then(() => {
